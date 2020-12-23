062330b71ea2e59c516f32ae90210868fdc9a79a
Jinker <2331423373@qq.com>
2020-12-23 12:32:59 +0800

    4.3.1

---
+++ Src/Client/Assets/Scripts/UI/UILogin.cs
@@ -0,0 +1,38 @@
+﻿using Services;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILogin : MonoBehaviour
+{
+    public InputField username;
+    public InputField password;
+    public Button buttonLogin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickLogin()
+    {
+        if (string.IsNullOrEmpty(this.username.text))
+        {
+            MessageBox.Show("请输入账号");
+            return;
+        }
+        if (string.IsNullOrEmpty(this.password.text))
+        {
+            MessageBox.Show("请输入密码");
+            return;
+        }
+        //UserService.Instance.SendLogin(this.username.text, this.password.text);
+    }
+}
